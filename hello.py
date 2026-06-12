d4dc7cc36c80efd69ae17f036d50b7ce1af82659
mariomorante <104558329+mariomorante@users.noreply.github.com>
2026-06-12 08:09:02 +0000

    Otro cambio

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
 #Hola Mundo
 print("Hola Python")
+
+
+"""
+Esto es un comentario de varias lineas
+que se puede usar para explicar el código o para escribir notas
+"""
+'''
+Esto también es un comentario de varias lineas
+que se puede usar para explicar el código o para escribir notas
+'''
